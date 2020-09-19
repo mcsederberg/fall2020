49c3d8e22995dddced523ea0f1ecdfc9af625dd9
Nicole Curtis <nicoleac@gmail.com>
2020-09-18 22:03:46 -0600

    Set up vue!

---
+++ frontend/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+<div id="app">
+	<div id="header">
+		<div class="title">
+			Forklift
+		</div>
+	</div>
+	<div id="mainWrapper">
+		<div id="sidebar">
+			<router-link to="/" class="sidebarOption">Home</router-link>
+			<router-link to="/projects" class="sidebarOption">Projects</router-link>
+			<router-link to="/tasks" class="sidebarOption">Tasks</router-link>
+			<router-link to="/chart" class="sidebarOption">Gantt chart</router-link> 
+		</div>
+		<router-view/>
+	</div>
+</div>
+</template>
+
+
+<style>
+#mainWrapper{
+    display: flex;
+    background: #242424;
+}
+#sidebar{
+    width: 235px;
+    color: white;
+    background: #353535;
+    display: flex;
+    flex-direction: column;
+    padding-top: 10px;
+}
+.sidebarOption{
+    padding: 10px 20px;
+    cursor: pointer;
+    color: white;
+	font-size: 18px;
+	text-decoration: none;
+}
+.sidebarOption:hover{
+    background-color: #242424;
+}
+
+#sidebar a.router-link-exact-active {
+    background-color: #242424;
+}
+.title{
+    margin: auto 0px;
+    font-size: 30px;
+}
+#header{
+    height: 63px;
+    border-bottom: 1px solid #a4a4a4;
+    padding-left: 40px;
+    background: #242424;
+    color: white;
+    display: flex;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="content">
+    <div id="mainWrapper">
+		<div id="body">
+			<p>
+				Welcome to Forklift! Go to <a href="/projects">Projects</a> to create a new project to work on with your group
+			</p>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Home',
+	components: {
+	}
+}
+</script>
+
