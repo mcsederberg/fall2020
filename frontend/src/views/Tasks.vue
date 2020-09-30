33e26606f9aea2728fc3306958d1699488c2b3cf
mcs1000 <mcsederberg@gmail.com>
2020-09-29 21:42:22 -0500

    Adding gantt chart stuff

---
+++ frontend/src/views/Tasks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="">
+      <div class="absolute">
+          <i class="fa fa-plus"/>
+      </div>
+    <h1>This is the Tasks page!</h1>
+  </div>
+</template>
+
+<script>
+// import api from '@/api'
+// import User from '../models/User'
+import Task from '../models/Task';
+export default {
+	name: 'Tasks',
+	components: {
+	},
+	data: function(){
+		return{
+
+		}
+	},
+	mounted: function(){
+    var foo = 0;
+	},
+	methods: {
+		
+	}
+}
+</script>
+
