40ac5ed6396837c594cb2e28c77d83f5f5f7fa0b
mcs1000 <mcsederberg@gmail.com>
2020-10-27 18:53:26 -0600

    Updating progress page

--- frontend/src/views/Chart.vue
+++ frontend/src/views/Chart.vue
@@ -1,21 +1,45 @@
 {{ src/components/Gantt.vue }}
 <template>
-  <div class="about w-full h-full" :style="aboutStyle">
-    <div class="w-full h-full" ref="gantt"></div>
-  </div>
+	<div class="about w-full h-full bg-lightBlue">
+		<div class="w-2/3 mx-auto h-full bg-darkBlue">
+			<div class="flex w-1/2 mx-auto pt-4">
+				<div class="text-xxxlg">My Tasks</div>
+			</div>
+			<div class="flex w-1/2 mx-auto pl-4 pt-4">
+				<div class="text-xxlg">Gantt Chart</div><span @click="chartIsBig=!chartIsBig" class="ml-2 self-center text-2xs text-gray">(Click to enlarge)</span>
+			</div>
+			<div @click="chartIsBig=!chartIsBig">
+				<div :class="chartClasses" ref="gantt" :style="aboutStyle"></div>
+			</div>
+			<div class="flex w-1/2 mx-auto pl-4 pt-4">
+				<div class="text-xxlg">Total Team Hours: {{teamHours}}</div>
+			</div>
+			<div class="flex w-1/2 mx-auto pl-4 pt-4">
+				<div  @click="collapsedHours=!collapsedHours" class="cursor-pointer text-xxlg">Individual Hours <i :class="['fa', {'fa-caret-down':collapsedHours, 'fa-caret-up':!collapsedHours}]"/></div>
+			</div>
+			<div v-for="hour in individualHours" :key="hour.id" class=" self-center w-1/2">  
+				{{hour.user}}:{{hour.time}}
+            </div>
+		</div>
+	</div>
 </template>
 
 <script>
 import {gantt} from 'dhtmlx-gantt';
 import Task from '../models/Task';
-import  Cookies from '../mixins/Cookies'
+import Hour from '../models/Hour';
+import Cookies from '../mixins/Cookies'
 export default {
 	name: 'Projects',
 	components: {
 	},
 	data: function(){
 		return{
-			displayTasks: {data: [], links: []}
+			displayTasks: {data: [], links: []},
+			chartIsBig: false,
+			collapsedHours: true,
+			teamHours: 0,
+			individualHours: []
 		}
 	},
 	mounted: function(){
@@ -34,7 +58,13 @@ export default {
 		aboutStyle: function(){
 			var rows = this.displayTasks.data.length + 1;
 			var height = rows * 35;
-			return "height: " + height + "px;";
+			return "min-height: " + height + "px;";
+		},
+		chartClasses: function() {
+			if (this.chartIsBig){
+				return "w-full mx-auto";
+			}
+			return "w-3/4 mx-auto pointer-events-none";
 		}
 	},
 	methods: {
@@ -62,9 +92,18 @@ export default {
 				var code = e.error;
 				switch (code){
                     default:
-					// case "ER_DUP_ENTRY":
-					// 	alert("That username is taken");
-					// 	break;
+				}
+			});
+		},
+		getTeamHours: function(){
+            var vue = this;
+			var res = Hour.getHoursForProject(this.project.id);
+			res.then(function(response){
+				vue.teamHours = response;
+			}).catch(function(e){
+				var code = e.error;
+				switch (code){
+                    default:
 				}
 			});
 		},
