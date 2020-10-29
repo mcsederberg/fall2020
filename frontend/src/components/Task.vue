44fa60cec5c435efe4dbefb628a5b4b2ad40fa8f
Nicole Curtis <nicoleac@gmail.com>
2020-10-28 20:12:33 -0600

    Made mixin, progress with progress bar

--- frontend/src/components/Task.vue
+++ frontend/src/components/Task.vue
@@ -11,7 +11,16 @@
 		<div class="taskDescription ml-3 mt-3">
 			{{summary}}
 		</div>
-		<div class="self-end bg-orange rounded-lg cursor-pointer mt-1 py-1 px-3">Mark Complete</div>
+		<div class="flex flex-col lg:flex-row justify-between mt-2">
+			<span v-if="editPercent" @mouseover="hover = true" @mouseleave="hover = false" >
+				Percent Complete: 
+				<input type="range" min="0" max="100" v-model="percentComplete" class="slider bg-white" id="myRange" @mousedown="willChange" @mouseup="doneChanging">
+				<span v-if="hover">{{percentComplete}}%</span>
+				<!-- <span v-if="changing">For testing</span> -->
+			</span>
+			<span v-else/> <!--empty span for alignment-->
+			<div class="self-end bg-orange rounded-lg cursor-pointer mt-1 py-1 px-3">Mark Complete</div>
+		</div>
 	</div>
 
 </div>
@@ -31,6 +40,18 @@ export default {
 		summary: {
 			type: String,
 			required:true
+		},
+		editPercent: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data: function() {
+		return {
+			percentComplete: 0,
+			hover:false,
+			changing: false,
+			percentBefore: null
 		}
 	},
 	methods: {
@@ -38,13 +59,42 @@ export default {
 			this.$emit("deleted")
 		},
 		prettyDate: function(dateString){
-            var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-            var date = new Date(dateString);
-            var monthIndex = date.getMonth();
-            var day = date.getDate();
-            var year = date.getFullYear();
-            return months[monthIndex] + " " + day + ", " + year;
-        },
+			var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+			var date = new Date(dateString);
+			var monthIndex = date.getMonth();
+			var day = date.getDate();
+			var year = date.getFullYear();
+			return months[monthIndex] + " " + day + ", " + year;
+		},
+		willChange: function() {
+			this.percentBefore = this.percentComplete;
+			this.changing = true;
+		},
+		doneChanging: function() {
+			this.changing = false;
+			let valueToSet = this.percentComplete;
+			if (valueToSet != this.percentBefore) {
+				this.$emit("savePercent", valueToSet);
+			}
+		}
 	}
 }
 </script>
+
+<style scoped>
+.slider {
+	-webkit-appearance: none;
+	height: 2px;
+	border-radius: 5px;
+	outline: none;
+}
+.slider::-webkit-slider-thumb {
+	-webkit-appearance: none;
+	appearance: none;
+	border-radius: 50%;
+	width: 20px;
+	height: 20px;
+	background: white;
+	cursor: pointer;
+}
+</style>
--- frontend/src/views/TeamTasks.vue
+++ frontend/src/views/TeamTasks.vue
@@ -42,167 +42,26 @@
 <script>
 import Task from '../models/Task';
 import Popup from '../components/Popup';
-import Cookies from '../mixins/Cookies';
+// import Cookies from '../mixins/Cookies';
 import T from '../components/Task'
+import taskMixin from '../mixins/taskMixin'
 export default {
 	name: 'Tasks',
 	components: {
         Popup:Popup,
         Task:T
-	},
+    },
+    mixins: [taskMixin],
 	data: function(){
 		return{
-            showPopup: false,
-            popupTask: {
-                title: "",
-                summary: "",
-                dueDate: "",
-                completedDate: "",
-                status: "open",
-                percentComplete: "60",
-                startDate: "",
-                deleted: false,
-            },
-            allTasks: [],
-            projectHours: 0,
-            popupType: null,
-            ACTIVITY_CREATE: 0,
-            ACTIVITY_EDIT: 1,
-            clockedIn: false,
-            user: {},
-            project: {},
+            
         }
     },
     mounted: function(){
         this.getCookies();
         this.getAllTasks();
     },
-    computed: {
-        sortedTasks: function(){
-            return this.allTasks.slice().sort((a,b) => new Date(b.start_date) - new Date(a.start_date));
-        }
-    },
-    methods: {
-        getCookies: function(){
-            this.user = Cookies.getUser();
-            this.project = Cookies.getProject();
-        },
-        SQLDateTime: function(date){
-            if (date == ""){
-                return "";
-            }
-            return new Date(date).toISOString().slice(0, 19).replace('T', ' ');
-        },
-        taskPopupFunction: function(){
-            if (this.popupType === this.ACTIVITY_CREATE){
-                this.createTask();
-            } else {
-                this.updateTask();
-            }
-        },
-        editActivityPopup: function(taskInput){
-            this.popupType = this.ACTIVITY_EDIT;
-            var task = taskInput.duplicate();
-            task.completedDate = this.popupDate(task.completedDate);
-            task.dueDate = this.popupDate(task.dueDate);
-            task.startDate = this.popupDate(task.startDate);
-            this.popupTask = task;
-            this.showPopup = true;
-        },
-        updateTask: function(){
-            if (this.popupTask.title == "" 
-            || this.popupTask.summary == "" 
-            || this.popupTask.dueDate == "" 
-            || this.popupTask.status == ""
-            || this.popupTask.startDate == ""){
-                alert("You must fill in all of the fields");
-                return;
-            }
-            var res = Task.updateTask(
-                this.popupTask.id, 
-                this.user.id, 
-                this.project.id,
-                this.popupTask.title, 
-                this.popupTask.summary,
-                this.SQLDateTime(this.popupTask.dueDate), 
-                this.SQLDateTime(this.popupTask.startDate), 
-                this.SQLDateTime(this.popupTask.completedDate), 
-                this.popupTask.status, 
-                this.popupTask.percentComplete
-            );
-            var vue = this;
-            res.then(function(response){
-                for (var i in vue.allTasks) {
-                    if (vue.allTasks[i].id == response.id) {
-                        vue.$set(vue.allTasks, i, response);
-                        break;
-                    }
-                }
-                vue.showPopup = false;
-            }).catch(function(e){
-                var code = e.error;	
-                switch (code){
-                    default:
-                    // case "INCORRECT_PASSWORD":
-                    // 	alert("Incorrect password");
-                    // 	break;
-                    // case "INVALID_USERNAME":
-                    // 	alert("Invalid username");
-                    // 	break;
-                }
-            });
-        },
-        createTaskPopup: function(){
-            this.popupType = this.ACTIVITY_CREATE;
-            this.popupTask = {
-                title: "",
-                summary: "",
-                dueDate: "",
-                completedDate: "",
-                status: "open",
-                percentComplete: "60",
-                startDate: "",
-                deleted: false,
-            };
-            this.showPopup = true;
-        },
-        createTask: function(){
-            if (this.popupTask.title == "" 
-            || this.popupTask.summary == "" 
-            || this.popupTask.dueDate == "" 
-            || this.popupTask.status == ""
-            || this.popupTask.startDate == ""){
-                alert("You must fill in all of the fields");
-                return;
-            }
-            var res = Task.createTask(
-                this.user.id, 
-                this.project.id, 
-                this.popupTask.title, 
-                this.popupTask.summary, 
-                this.SQLDateTime(this.popupTask.dueDate), 
-                this.SQLDateTime(this.popupTask.startDate), 
-                this.SQLDateTime(this.popupTask.completedDate), 
-                this.popupTask.status, 
-                this.popupTask.percentComplete
-            );
-            var vue = this;
-            res.then(function(response){
-                vue.allTasks.push(response);
-                vue.showPopup = false;
-            }).catch(function(e){
-                var code = e.error;	
-                switch (code){
-                    default:
-                    // case "INCORRECT_PASSWORD":
-                    // 	alert("Incorrect password");
-                    // 	break;
-                    // case "INVALID_USERNAME":
-                    // 	alert("Invalid username");
-                    // 	break;
-                }
-            });
-        },
+    methods: { //see mixin for more
         getAllTasks: function(){
             var vue = this;
             var res = Task.getTasksForProjectID(this.project.id);
@@ -218,34 +77,6 @@ export default {
                     // 	break;
                 }
             });
-        },
-        deleteTask: function(taskID){
-            var vue = this;
-            var res = Task.deleteTask(taskID);
-            res.then(function(){
-                vue.allTasks = vue.allTasks.filter(function(task){
-                    return task.id !== taskID;
-                });
-            }).catch(function(e){
-                var code = e.error;
-                switch (code){
-                    default:
-                    // case "ER_DUP_ENTRY":
-                    // 	alert("That username is taken");
-                    // 	break;
-                }
-            });
-        },
-        prettyDate: function(dateString){
-            var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-            var date = new Date(dateString);
-            var monthIndex = date.getMonth();
-            var day = date.getDate();
-            var year = date.getFullYear();
-            return months[monthIndex] + " " + day + ", " + year;
-        },
-        popupDate: function(dateString){
-            return new Date(dateString).toString("MMM dd yyyy");
         },
     }
 }
