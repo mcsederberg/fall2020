f29331f832b47aceafe17af0c4dfa9454890351d
Nicole Curtis <nicoleac@gmail.com>
2020-11-24 10:34:22 -0700

    stub function test

---
+++ frontend/__tests__/views/MyTasksTest.js
@@ -0,0 +1,25 @@
+import myTasks from '../../src/views/MyTasks.vue';
+
+import { mount } from '@vue/test-utils'
+
+let taskWrapper;
+
+beforeEach(() => {
+	myTasks.methods.getMyTasks = jest.fn() //stub function. -> is set here since it's called in mounted. Otherwise I could have done `taskWrapper.vm.getMyTasks = jest.fn();` down below
+    taskWrapper = mount(myTasks);
+});
+
+afterEach(() => {
+    taskWrapper.destroy();
+});
+
+
+test('calls getMyTasks', () => {
+	expect(myTasks.methods.getMyTasks.mock.calls.length).toEqual(1);
+})
+
+// test('calls getMyTasks', () => {
+// 	expect(myTasks.methods.getMyTasks.mock.calls.length).toEqual(1);
+// })
+
+
