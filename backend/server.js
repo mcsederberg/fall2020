dfc2a5f06ab5a90f2e44324e8a8483f96fd696b6
mcs1000 <mcsederberg@gmail.com>
2020-11-29 12:24:49 -0600

    Adding mongo stuff to backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,55 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(express.static('public'));
+const mongoose = require('mongoose');
 const mysql = require('mysql');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/forkliftDev', {
+	useNewUrlParser: true
+  });
+
+
+// // Create a scheme for products in the museum: a title and a path to an image.
+// const saveSchema = new mongoose.Schema({
+// 	name: String,
+// 	board: [
+// 	  [{
+// 		type: String
+// 	  }]
+// 	],
+// 	score: String
+//   });
+//   // Create a model for products in the museum.
+//   const Save = mongoose.model('Save', saveSchema);
+
+//   const save = new Save({
+//     name: "MichaelS",
+//     board: "boardd",
+//     score: "scoreSave"
+//   });
+//   try {
+//     save.save();
+//     console.log("Saved")
+//   } catch (error) {
+//     console.log(error);
+//     console.log("Not saved")
+//   }
+
+  
+//   async function testStuff(){
+// 	try {
+// 		let saves = await Save.find();
+// 		console.log("Saves:");
+// 		console.log(saves);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+
+//   }
+//   testStuff()
+
+  
 const { time } = require('console');
 
 const user = require("./serverFiles/user.js");
