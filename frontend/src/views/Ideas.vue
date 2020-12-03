d7cf5d4f1168fb851625269c73bdc470f2b04911
Carachips <carachips4@gmail.com>
2020-12-02 21:26:12 -0700

    added Message component

--- frontend/src/views/Ideas.vue
+++ frontend/src/views/Ideas.vue
@@ -5,21 +5,25 @@
 			<div class="flex w-1/2 mx-auto">
 				<div class="text-xxxlg font-sans">Idea Board</div>
 			</div>
+			<Message :message="message"/>
 		</div>
 	</div>
 </template>
 
 <script>
 import Cookies from '../mixins/Cookies'
+import Message from '../components/Message';
 export default {
 	name: 'IdeaBoard',
 	components: {
+		Message: Message
 	},
 	data: function(){
 		return{
 			user: {},
 			project: {},
-			projectUsers: []
+			projectUsers: [],
+			message: {content: "Here is the content to be shared", userID: "Michael", timePublished: "2020-12-07T07:00:00.000Z", editDate: "2020-12-07T07:00:00.000Z", priority: true}
 		}
 	},
 	mounted: function(){
